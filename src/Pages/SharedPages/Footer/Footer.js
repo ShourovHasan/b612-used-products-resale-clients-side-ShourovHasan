ee5e43368fb70c7442bfc1540446bf8c146c04d2
ShourovHasan <shourovhasan@gmail.com>
2022-11-25 11:23:53 +0600

    Footer added

--- src/Pages/SharedPages/Footer/Footer.js
+++ src/Pages/SharedPages/Footer/Footer.js
@@ -4,7 +4,29 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='pb-10 text-white bg-black'>
-            
+            <footer className="justify-around p-10 footer ">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </div>
+            </footer>
+            <p className='text-center'>Copyright 2022 All Rights Reserved</p>
         </div>
     );
 };
