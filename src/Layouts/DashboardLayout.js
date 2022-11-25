cc768a775ab41322a2805b083a669af01c3e4296
ShourovHasan <shourovhasan@gmail.com>
2022-11-25 11:30:14 +0600

    Dashboard layout page added

--- src/Layouts/DashboardLayout.js
+++ src/Layouts/DashboardLayout.js
@@ -8,11 +8,45 @@ import Header from '../Pages/SharedPages/Header/Header';
 
 
 const DashboardLayout = () => {
-    
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
+    const [isSeller] = useSeller(user?.email);
+    const [isBuyer] = useBuyer(user?.email);
 
     return (
         <div>
-            
+            <Header></Header>
+            <div className="drawer drawer-mobile ">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="flex flex-col drawer-content">
+                    <Outlet></Outlet>
+                </div>
+                <div className="w-48 drawer-side">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <ul className="p-4 menu w-80 text-base-content ">
+                        {/* <!-- Sidebar content here --> */}
+                        {
+                            isAdmin && <>
+                                <li><Link to='/dashboard/allBuyers'>All Buyers</Link></li>
+                                <li><Link to='/dashboard/allSellers'>All Sellers</Link></li>
+                            </>
+                        }
+                        {
+                            isSeller && <>
+                                <li><Link to='/dashboard/myProducts'>My Products</Link></li>
+                                <li><Link to='/dashboard/addProduct'>Add Product</Link></li>                                
+                            </>
+                        }
+                        {
+                            isBuyer && <>
+                                <li><Link to='/dashboard/myOrders'>My Orders</Link></li>
+                                <li><Link to='/dashboard/myWishlist'>My Wishlist</Link></li>                                
+                            </>
+                        }
+                    </ul>
+
+                </div>
+            </div>
 
         </div>
     );
