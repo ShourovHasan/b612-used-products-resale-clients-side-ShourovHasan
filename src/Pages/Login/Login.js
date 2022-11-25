df1aaff58f19e1e9cef11b98827eea256b2c9cea
ShourovHasan <shourovhasan@gmail.com>
2022-11-25 11:28:07 +0600

    Login page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,11 +6,123 @@ import { useForm } from "react-hook-form";
 import useToken from '../../hooks/useToken';
 
 const Login = () => {
-    
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signInWithPassword, googleSignIn } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+    const [token] = useToken(loginUserEmail);
+
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    if (token) {
+        navigate(from, { replace: true });
+    }
+
+    // sign in with email and password 
+    const handleLogin = data => {
+        const { email, password } = data;
+        console.log(data);
+        console.log(errors);
+
+        setLoginError('');
+        handleSignInWithPassword(email, password);
+
+    }
+
+    const handleSignInWithPassword = (email, password) => {
+        signInWithPassword(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setLoginUserEmail(email);
+                toast.success('Login Successful');
+            })
+            .catch(error => {
+                console.error(error.message);
+                setLoginError(error.message);
+            })
+    }
+    // sign in with google 
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                const userType = {
+                    userType: 'buyer'
+                }                
+                saveUser(user.displayName, user.email, userType.userType)                
+            })
+            .catch(error => {
+                console.error(error.message);
+                setLoginError(error.message);
+            })
+    }
+    const saveUser = (displayName, email, userType) => {
+        const user = { displayName, email, userType };
+        console.log(user);
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setLoginUserEmail(email);
+                toast.success('Login Successful');
+                // navigate(from, { replace: true });
+                // getUserToken(email)
+                // console.log('Save user', data);
+            })
+    }
 
     return (
         <div className='h-[800px] flex justify-center items-center flex-col'>
-            
+            <div className='px-5 rounded-lg shadow-xl shadow-neutral py-7 w-[385px]'>
+                <h2 className='mb-5 text-xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)} className=''>
+                    {/* <Header /> */}
+                    <div className="w-full form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email", {
+                            required: "Email address is required"
+                        })} type="email" placeholder="email" className="w-full input input-bordered" />
+                        {errors.email && <p role="alert" className='ml-4 text-red-500'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="w-full mt-2 form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: 'Password at least 6 characters' },
+                            maxLength: 20
+                        })} type="password" placeholder="password" className="w-full input input-bordered " />
+                        {errors.password && <p role="alert" className='ml-4 text-red-500'>{errors.password?.message}</p>}
+                        <label className="label">
+                            <Link to='/reset' className="label-text">Forget Password?</Link>
+                        </label>
+                    </div>
+                    <input type="submit" className='w-full pb-0 mt-5 mb-0 text-accent btn' value='Login' />
+                    <div>
+                        {loginError && <p className='ml-4 text-red-500'>{loginError}</p>}
+                    </div>
+                </form>
+                <div className="flex flex-col w-full border-opacity-50">
+                    <div className="grid w-full h-20 py-0 my-0 card rounded-box place-items-center"><p>New to this Store? <Link to='/register' className='text-primary'>Create new Account</Link></p>
+                    </div>
+                    <div className="py-0 my-0 divider">OR</div>
+                    <div className="grid h-20 py-0 my-0 card rounded-box place-items-center">
+                        <button onClick={handleGoogleSignIn} className='w-full btn btn-outline'>CONTINUE WITH GOOGLE</button>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
