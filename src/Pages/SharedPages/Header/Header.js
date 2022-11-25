dadc5f70c7a33e6d98b1456dd0c6277a8f00a1b0
ShourovHasan <shourovhasan@gmail.com>
2022-11-25 11:21:13 +0600

    Header navbar added

--- src/Pages/SharedPages/Header/Header.js
+++ src/Pages/SharedPages/Header/Header.js
@@ -31,7 +31,25 @@ const Header = () => {
     </React.Fragment>
     return (
         <div className="navbar bg-base-100 text-neutral">
-            
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={1} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/' className="text-xl normal-case btn btn-ghost">Resale Mobile Store</Link>
+            </div>
+            <div className="hidden navbar-end lg:flex">
+                <ul className="p-0 menu menu-horizontal">
+                    {menuItems}
+                </ul>
+            </div>
+            <label tabIndex={2} htmlFor="dashboard-drawer" className="navbar-end btn btn-ghost lg:hidden ">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
         </div>
     );
 };
