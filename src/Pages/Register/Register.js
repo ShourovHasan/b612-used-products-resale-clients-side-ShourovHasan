d465a59d2dad3eaa13af6e1a790372fe1a809b2c
ShourovHasan <shourovhasan@gmail.com>
2022-11-25 11:25:48 +0600

    Registration page added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -32,7 +32,7 @@ const Register = () => {
         createUser(email, password)
             .then(result => {
                 const user = result.user;
-                // toast.success('User Created Successfully');
+                toast.success('User Created Successfully');
                 const userInfo = {
                     displayName: displayName
                 }
@@ -69,11 +69,10 @@ const Register = () => {
             .then(data => {
                 if (data.message) {
                     setCreatedUserEmail(email);
-                    toast.error(data.message);
-                    return toast.success('User Login Successfully');
+                    return toast.error(data.message);
                 }
                 setCreatedUserEmail(email);
-                toast.success('User Created Successfully');
+                toast.success('User created Successfully');
                 // getUserToken(email)
                 // console.log('Save user', data);
             })
@@ -97,7 +96,64 @@ const Register = () => {
     }
     return (
         <div className='h-[800px] flex justify-center items-center flex-col'>
-            
+            <div className='px-5 rounded-lg shadow-xl shadow-neutral py-7 w-[385px]'>
+                <h2 className='mb-5 text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleRegister)} className=''>
+                    {/* <Header /> */}
+                    <div className="w-full form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input {...register("displayName", {
+                            required: "full name is required"
+                        })} type="text" placeholder="full name" className="w-full input input-bordered" />
+                        {errors.displayName && <p className='ml-4 text-red-500'>{errors.displayName.message}</p>}
+                    </div>
+                    <div className="w-full form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input  {...register("email", {
+                            required: "email address is required"
+                        })} type="email" placeholder="email" className="w-full input input-bordered" />
+                        {errors.email && <p role="alert" className='ml-4 text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="w-full mt-2 form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input  {...register("password", {
+                            required: "password is required",
+                            minLength: { value: 6, message: 'password at least 6 characters' },
+                            maxLength: 20,
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/, message: 'password must have Uppercase, lowercase, number and Special characters' }
+                        })} type="password" placeholder="password" className="w-full input input-bordered " />
+                        {errors.password && <p role="alert" className='ml-4 text-red-600'>{errors.password?.message}</p>}
+                    </div>
+                    <div className="w-full form-control">
+                        <label className="label">
+                            <span className="label-text">User Type</span>
+                        </label>
+                        <select {...register("userType")} className="w-full select input-bordered">
+                            <option>buyer</option>
+                            <option>seller</option>
+                        </select>
+                    </div>
+                    <input type="submit" className='w-full pb-0 mt-5 mb-0 text-accent btn' value='Sign Up' />
+                    <div>
+                        {
+                            signUpError &&
+                            <p className='ml-4 text-red-500'>{signUpError}</p>
+                        }
+                    </div>
+                </form>
+                <div className="flex flex-col w-full border-opacity-50">
+                    <div className="grid w-full h-20 py-0 my-0 card rounded-box place-items-center"><p>Already have an account? <Link to='/login' className='text-primary'>Please Login</Link></p>
+                    </div>
+                    <div className="py-0 my-0 divider">OR</div>
+                    <div className="grid h-20 py-0 my-0 card rounded-box place-items-center"><button onClick={handleSignInWithGoogle} className='w-full btn btn-outline'>CONTINUE WITH GOOGLE</button></div>
+                </div>
+            </div>
 
         </div>
     );
